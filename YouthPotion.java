09185c98493b9e0725cba12e9c40320f5a300811
340802040 <101133854+340802040@users.noreply.github.com>
2022-11-01 14:08:07 -0400

    Wrote description

--- YouthPotion.java
+++ YouthPotion.java
@@ -1,17 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class YouthPotion here.
+ * Grandmas return to their prime. Their speed of production increases by 2x.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Jonathan Zhao 
+ * @version 1.0 (November 1)
  */
 public class YouthPotion extends PowerUp
 {
-    /**
-     * Act - do whatever the YouthPotion wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         // Add your action code here.
