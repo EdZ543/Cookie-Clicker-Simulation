09e4c0c466732f112b95f3891f3f10abe361aea4
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2022-11-02 13:43:18 -0400

    Resize world so it fits on my screen

--- MyWorld.java
+++ MyWorld.java
@@ -8,12 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
     // World variables
     GreenfootImage background;
     public MyWorld()
     {    
-        super(1280, 800, 1); 
+        super(1200, 800, 1); 
         // Set world background
         background = new GreenfootImage("background0.png");
         setBackground(background);
