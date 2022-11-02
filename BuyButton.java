e166dccc0acff3d0e96d6914287da2d2586f9906
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2022-11-02 14:07:45 -0400

    Add author name to Building

--- BuyButton.java
+++ BuyButton.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The rectangles in which the buildings will stay
+ * Write a description of class BuyButton here.
  * 
- * @author Eddie
+ * @author (your name) 
  * @version (a version number or a date)
  */
-public class BuildingRow extends Actor
+public class BuyButton extends Actor
 {
     /**
-     * Act - do whatever the BuildingRow wants to do. This method is called whenever
+     * Act - do whatever the BuyButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
+ * Credits:
+ * 
+ * 
  * @author (your name) 
  * @version (a version number or a date)
  */
