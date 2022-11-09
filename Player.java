8b04cdcddbb904e34cd5878c3b7e6ca8616035fe
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2022-11-09 14:38:26 -0500

    Flesh out player api

--- Player.java
+++ Player.java
@@ -1,24 +1,51 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Player here.
+ * A player in this simulation, of which there are two.
+ * Adjustable properties:
+ * - Clicks per second
+ * - Number of clickers
+ * - Number of starting grandmas
  * 
- * @author Patrick Hu
+ * @author Eddie Zhuang
  * @version November 2022
  */
 public class Player extends Actor
 {
-    private int numCookies = 0;
+    private Cookie cookie;
     
-    public Player() {
+    private int numCookies = 0;
+    private int clickers;
+    private int cps;
+    private int grandmas;
+    
+    /**
+     * @param clickers How many clickers the player will start off with
+     * @param cps The number of clicks per second each clicker will be able to have
+     * @param grandmas  Number of starting grandmas
+     */
+    public Player(int clickers, int cps, int grandmas) {
+        this.clickers = clickers;
+        this.cps = cps;
+        this.grandmas = grandmas;
+        
         // Set image to none
         setImage((GreenfootImage)null);
-        
+    }
+    
+    public void addedToWorld(World w) {
         // Add cookie
+        cookie = new Cookie();
+        w.addObject(cookie, getX(), getY() - 400);
         
         // Add clickers
+        for (int i = 0; i < clickers; i++) {
+            
+        }
         
         // Add building rows
+        
+        // Add starting grandmas
         
         // Add score text
     }
@@ -27,10 +54,20 @@ public class Player extends Actor
         
     }
     
-    public void setCookieCount(int x) {
-        numCookies = x;
+    /**
+     * Change the amount of cookies the player has.
+     * 
+     * @param x The amount to change by. If negative, it takes away cookies.
+     */
+    public void changeCookieCount(int x) {
+        numCookies += x;
     }
     
+    /**
+     * Returns the amount of cookies that the player has.
+     * 
+     * @return int The number of cookies
+     */
     public int getCookieCount() {
         return numCookies;
     }
