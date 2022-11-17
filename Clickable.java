2f86aab42a4d91a05a6a3c77a50f133245f4a76d
EdZ543 <53500062+EdZ543@users.noreply.github.com>
2022-11-17 14:44:05 -0500

    Move getRandomNumberinRange higher up

--- Clickable.java
+++ Clickable.java
@@ -14,4 +14,12 @@ public abstract class Clickable extends Actor
      * @param player The player clicking this object
      */
     public void click(Player player) {}
+    
+    /**
+     * Gets a random number in the range from `start` to `end` inclusive.
+     */
+    protected int getRandomNumberInRange(int start, int end) {
+       int a = Greenfoot.getRandomNumber(end - start + 1);
+       return start + a;
+    }
 }
