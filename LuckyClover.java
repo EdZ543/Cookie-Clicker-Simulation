5708078b1e2a0daa419a337ddf02b66bfb355b19
gpot <mykkchan@gmail.com>
2022-11-18 23:20:10 -0500

    LuckyClover logic
    commented out for now

--- LuckyClover.java
+++ LuckyClover.java
@@ -11,8 +11,19 @@ public class LuckyClover extends Powerup
     public LuckyClover(Player origin) {
         super(origin);
     }
+    
+    public void addedToWorld(World w) {
+        // Building.LUCKY = true;
+    }
         
     public void act() {
+        actCount ++;
+        if(actCount == duration*60) {
+            // Building.LUCKY = false;
+            getWorld().removeObject(this);
+        }
+    }
+    public void animate() {
         
     }
 }
